d47b1d2079cf9d5ed236b9e26ea8454d77ae7c73
unknown <kudashevtim@gmail.com>
2023-11-20 16:41:48 +0300

    date

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="box">
       <section>
-      <p>footer slot</p>
+        <div class="tooltip">{{ interval }} {{ interval === 1 ? " сутки" : " суток"}}</div><!-- /.tooltip -->
+
       <div class='dpicker__wrapper'>
-        <input type="text" @focus='open = !open' v-model='value3'>
-        <input type="text" @focus='open = !open' v-model='value3'>
-        <date-picker v-model:value="value3" range placeholder="Select date range" v-model:open="open">
+        <input type="text" @focus='open = !open' v-model='startdate'>
+        <input type="text" @focus='open = !open' v-model='enddate'>
+        <date-picker @close='handeClose' v-model:value="value3" range placeholder="Select date range" v-model:open="open">
           <template #footer="{ emit }">
             <div style="text-align: left">
-              <button class="mx-btn mx-btn-text" @click="selectNextThreeDay(emit)">
-                Ближайшие 3 дня
+              <button class="mx-btn mx-btn-text" @click="selectToday(emit)">
+                Сегодня
               </button>
               <button class="mx-btn mx-btn-text" @click="selectTomorrow(emit)">
                 Завтра
+              </button>
+              <button class="mx-btn mx-btn-text" @click="selectWeekend(emit)">
+                Выходные
               </button>
             </div>
           </template>
@@ -25,6 +29,10 @@
 </template>
 
 <script>
+import moment from 'moment'
+
+import 'vue-datepicker-next/locale/ru';
+
 import DatePicker from 'vue-datepicker-next';
   import 'vue-datepicker-next/index.css';
 
@@ -33,30 +41,102 @@ import DatePicker from 'vue-datepicker-next';
     data() {
       return {
         value3: [null, null],
-        time0: null,
-        time1: null,
-        time2: null,
-        time3: null,
+        intervalvalue: 0,
         open: false,
+        langString: 'ru',
     };
     },
+    computed: {
+      startdate() {
+        let sdate = this.value3[0]
+        return sdate ?  moment(sdate).format('DD MMMM, dd') : ""
+      },
+      enddate() {
+        let edate = this.value3[1]
+        return edate ?  moment(edate).format('DD MMMM, dd') : ""
+      },
+      interval() {
+       
+        return this.intervalvalue
+      }
+    },
+    mounted() {
+      moment.locale('ru');
+      let _this = this
+      document.addEventListener("mouseover", function(e){
+        let int = null
+        if (int) {
+          clearTimeout(int)
+        }
+        const target = e.target.closest(".cell") ? e.target.closest(".cell") : e.target
+        if(target ){
+          int = setTimeout(() => {
+            _this.intervalvalue = document.querySelectorAll('td.cell.hover-in-range:not(.not-current-month)').length + 1; 
+
+            let active = document.querySelectorAll('.cell.active:not(.not-current-month)').length; 
+
+            if (_this.intervalvalue > 0 && active === 1) {
+              var x = target.getBoundingClientRect().left 
+              var y = target.getBoundingClientRect().top 
+            } else {
+               x = -100
+               y = -100
+            }
+            const tooltip = document.querySelector('.tooltip')
+              tooltip.style.top = y + 'px'
+              tooltip.style.left = x + 'px'
+          }, 100)
+        }  
+      });
+    },
     methods: {
-    selectNextThreeDay(emit) {
-      const start = new Date();
-      const end = new Date();
-      end.setTime(end.getTime() + 3 * 24 * 3600 * 1000);
-      const date = [start, end];
-      emit(date);
+       getNextSunday(date = new Date()) {
+        const dateCopy = new Date(date.getTime());
+
+        const nextSunday = new Date(
+          dateCopy.setDate(
+            dateCopy.getDate() + ((7 - dateCopy.getDay() + 6) % 7 || 7),
+          ),
+        );
+
+        return nextSunday;
+      },
+      selectToday(emit) {
+        const start = new Date();
+        const end = new Date();
+        end.setTime(end.getTime() + 1 * 24 * 3600 * 1000);
+        const date = [start, end];
+        emit(date);
+      },
+      selectTomorrow(emit) {
+        const start = new Date();
+        const end = new Date();
+        start.setTime(end.getTime() + 1 * 24 * 3600 * 1000);
+        end.setTime(end.getTime() + 2 * 24 * 3600 * 1000);
+        const date = [start, end];
+        emit(date);
+      },
+      selectWeekend(emit) {
+        const start = this.nextSunday
+        const end = new Date();
+        start.setTime(end.getTime());
+        end.setTime(end.getTime() + 1 * 24 * 3600 * 1000);
+        const date = [start, end];
+        emit(date);
+      },
+      handeClose() {
+        const tooltip = document.querySelector('.tooltip')
+              tooltip.style.top = -999999999 + 'px'
+              tooltip.style.left = -999999999 + 'px'
+      }
+     /*  handleOpen() {
+       
+        this.cells = document.querySelectorAll("td.cell")
+        this.cells.forEach(cell => cell.addEventListener('mouseover', function() {
+          this.intervalvalue = document.querySelectorAll('.hover-in-range').length;
+        }))
+      } */
     },
-    selectTomorrow(emit) {
-      const start = new Date();
-      const end = new Date();
-      start.setTime(end.getTime() + 1 * 24 * 3600 * 1000);
-      end.setTime(end.getTime() + 2 * 24 * 3600 * 1000);
-      const date = [start, end];
-      emit(date);
-    },
-  },
   };
 </script>
 
@@ -73,4 +153,34 @@ import DatePicker from 'vue-datepicker-next';
   .dpicker__wrapper {
     position: relative;
   }
+
+  .tooltip {
+  background-color: #545454;
+  -webkit-border-radius: 2px;
+  line-height: 1.1;
+  border-radius: 2px;
+  text-align: left;
+  color: #fff;
+  font-size: 11px;
+  padding: 6px 10px;
+  margin-bottom: 10px;
+  position: relative;
+  position: absolute;
+  left: -999999999px;
+  z-index: 9999;
+  top: -999999999px;
+  width: 51px;
+  white-space: normal;
+  transform: translateX(-22px) translateY(-20px);
+}
+/* .tooltip::after {
+  content: "";
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 4px solid #545454;
+  bottom: -4px;
+  left: 50%;
+  margin-left: -4px;
+  position: absolute;
+} */
 </style>
